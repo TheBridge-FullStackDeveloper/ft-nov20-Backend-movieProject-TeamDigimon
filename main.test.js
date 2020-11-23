6959d303f8256d6144f0fb02a8969bb17a30f053
tamarao <mara.o.montellano@gmail.com>
2020-11-23 15:19:29 +0100

    Test endpoint search

--- main.test.js
+++ main.test.js
@@ -1,39 +1,59 @@
 // dentro del require ponemos el nombre del archivo al que vamos a hacerle el test
 // ejemplo
 
-const suma = require('./main.js');
+const suma = require("./main.js");
 
-test('sumar 1 + 2 es igual a 3', () => {
+test("sumar 1 + 2 es igual a 3", () => {
 	expect(suma(1, 2)).toBe(3);
 });
 
 // a partir de aqui, tests:
 
 
-const checkPassword = require('.main.js');
-const checkUsers = require('.main.js');
-const popUp = require('.main.js');
+const checkPassword = require(".main.js");
+const checkUsers = require(".main.js");
+const popUp = require(".main.js");
 
-test('Aqui se valida el login', () => {
+test("Aqui se valida el login", () => {
 	expect(checkPassword(password.value).toBe(true));
 	expect(checkUsers(user.value).toBe(true));
 	expect(popUp().toBe("string"));
 });
 
-const registerUser = require('.main.js');
+const registerUser = require(".main.js");
 
 const user1 = {
-	email: "test1@test.com",
-	password: "Test12*."
-}
+	"email": "test1@test.com",
+	"password": "Test12*."
+};
 const user2 = {
-	email: "test1aest.com",
-	password: "Test12*."
-}
+	"email": "test1aest.com",
+	"password": "Test12*."
+};
 
-test('Aqui se valida el register', () => {
+test("Aqui se valida el register", () => {
 	expect(registerUser("aa@aa.aa").toBe(false));
 	expect(registerUser(user1.value).toBe(false));
 	expect(registerUser().toBe(false));
-})
+});
 
+// /SEARCH
+
+const search = require(".main.js");
+const objetoDatosMovies = {
+	"title": "movie1",
+	"img": "www.url.com/img",
+	"year": "aaaa",
+	"director": "director",
+	"category": "category",
+	"duration": "120min"
+};
+
+test("Validamos nuestro search", () => {
+
+	expect(search(objetoDatosMovies.value).tobe(true));
+
+});
+
+
+//
