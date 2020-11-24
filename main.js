a71c2af1d1f3d191c9ad8cd7661b0db9711d31dc
AbelAbbasi <abelyaesta@hotmail.com>
2020-11-24 16:36:45 +0100

    hola

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ function suma(a, b) {
 
 module.exports = {suma};
 
+///////////////////////SEARCH FETCH
+let title = "Tomates Verdes Fritos";
+let director = "cameron Diaz";
+fetch("http://localhost:8800/search", {
+	"method":"POST",
+	"headers":{"Content-Type": "application/json"},
+	"body": JSON.stringify({title, director})
+}).then(res=>{
+	let {title, img, premier, director, category, duration} = res.body;
+});
+
+//////////////////ADDMOVIE////////////////////
+let dataFromData = {
+	title,
+	"img" : "www.google.img",
+	"premier" : 1991-11-07,
+	director,
+	category,
+	duration
+};
+async function addMoviefun(dataFromData){
+
+}
