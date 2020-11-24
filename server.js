91269a13235cb2572afa61d435e35ff1f99ecb10
javiforis <javi.foris@hotmail.com>
2020-11-24 15:00:10 +0100

    comienzo JWT

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const CORS = require("cors");
+
+const bodyParser = require("body-parser");
+
+const base64 = require("base-64");
+
+const crypto = require("crypto");
+
+const PORT = 8080;
