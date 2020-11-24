262aae95ef6b08cc05b58362319534774534d9d5
tamarao <mara.o.montellano@gmail.com>
2020-11-24 14:59:01 +0100

    Comienzo OAuth

--- public/main.js
+++ public/main.js
@@ -1,10 +1,1 @@
-// ejemplo
-
-function suma(a, b) {
-	return a + b;
-}
-
-// de esta forma exportamos la funcion y podemos hacer "require" en otro archivo.js
-
-module.exports = {suma};
-
+// OAUTH
